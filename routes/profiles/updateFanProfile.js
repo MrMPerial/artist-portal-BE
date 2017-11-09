1c9c3ca9982bb1461502a79a5cf13eef182f4695
MrMPerial <christopher.mitchell26@gmail.com>
2017-11-08 20:38:04 -0600

    Endpoint to update fan profile

--- routes/profiles/updateFanProfile.js
+++ routes/profiles/updateFanProfile.js
@@ -1,7 +1,17 @@
+let Profile = require('../../models/profile.model');
+
 module.exports = {
   updateFanProfile
 }
 
-function updateFanProfile() {
-  console.log('Update Fan Profile Function');
+function updateFanProfile(profileToUpdate) {
+  return Profile.findById( profileToUpdate.id )
+  .then((profileFound) => {
+    profileFound.bannerImage = profileToUpdate.bannerImage;
+    profileFound.profileImage = profileToUpdate.profileImage;
+    profileFound.artistName = profileToUpdate.artistName;
+    profileFound.profileType = profileToUpdate.profileType;
+    profileFound.userName = profileToUpdate.userName;
+    return profileFound.save();
+  });
 }
