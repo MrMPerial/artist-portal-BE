30216af76afbe06e427f7da7862db3f47c0d57ac
MrMPerial <christopher.mitchell26@gmail.com>
2017-11-11 11:34:26 -0600

    Reverted last commit

--- routes/signup/addUser.js
+++ routes/signup/addUser.js
@@ -1,52 +1,51 @@
 let User = require('../../models/user.model');
-let bcrypt = require('bcrypt');
 
 module.exports = {
   addUser
 }
 
-function addUser(req, type) {
-  UserSchema.statics.authenticate = (email, password, callback) => {
-    User.findOne({ email: email })
-    .exec((err, user) => {
-      if (err) {
-        return callback(err);
-      } else if (!user) {
-        let err = new Error('User Not Found!');
-        err.status = 401;
-        return callback(err);
-      }
-      bcrypt.compare(password, user.password, (err, result) => {
-        if (result === true) {
-          return callback(null, user);
-        } else {
-          return callback();
-        }
-      })
-    });
-  }
-
-  UserSchema.pre('save', (next) => {
-    let user = this;
-    bcrypt.hash(user.password, 10, (err, hash) => {
-      if (err) {
-        return next(err);
-      }
-      user.password = hash;
-      next();
-    })
-  });
-}
-
 // function addUser(req, type) {
-//   let newUser = new User({
-//     email: req.body.email,
-//     firstname: req.body.firstname,
-//     lastname: req.body.lastname,
-//     username: req.body.username,
-//     password: req.body.password,
-//     profileType: type
-//   });
+//   UserSchema.statics.authenticate = (email, password, callback) => {
+//     User.findOne({ email: email })
+//     .exec((err, user) => {
+//       if (err) {
+//         return callback(err);
+//       } else if (!user) {
+//         let err = new Error('User Not Found!');
+//         err.status = 401;
+//         return callback(err);
+//       }
+//       bcrypt.compare(password, user.password, (err, result) => {
+//         if (result === true) {
+//           return callback(null, user);
+//         } else {
+//           return callback();
+//         }
+//       })
+//     });
+//   }
 //
-//   newUser.save();
+//   UserSchema.pre('save', (next) => {
+//     let user = this;
+//     bcrypt.hash(user.password, 10, (err, hash) => {
+//       if (err) {
+//         return next(err);
+//       }
+//       user.password = hash;
+//       next();
+//     })
+//   });
 // }
+
+function addUser(req, type) {
+  let newUser = new User({
+    email: req.body.email,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    username: req.body.username,
+    password: req.body.password,
+    profileType: type
+  });
+
+  newUser.save();
+}
--- routes/users/index.js
+++ routes/users/index.js
@@ -11,87 +11,87 @@ const deleteUser = require('./deleteUser');
 // User Login
 ///////////////////////////////////////////
 
-router.post('/login', (req, res, next) => {
-  if (req.body.password !== req.body.passwordConfirm) {
-    let err = new Error('Passwords do not match!');
-    err.status = 400;
-    res.send('Passwords do not match!');
-    return next(err);
-  }
-
-  if (req.body.email && req.body.username && req.body.password && req.body.passwordConfirm) {
-    let userData = {
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password,
-        passwordConfirm: req.body.passwordConfirm
-    }
-
-    User.create(userData, (err, user) => {
-      if (error) {
-        return next(error);
-      } else {
-        req.session.userId = user._id;
-        return res.redirect('/userProfile');
-      }
-    });
-  } else if (req.body.logemail, req.body.logpassword, (error, user) => {
-    User.authenticated(req.body.logemail, req.body.logpassword, (error, user) => {
-      if (error || !user) {
-        let err = new Error('Wrong email or password!');
-        err.status = 401;
-        return next(err);
-      } else {
-        req.session.userId = user._id;
-        return res.redirect('/userProfile');
-      } 
-    });
-  });
-});
-
-router.get('/userProfile', (req, res, next) => {
-  User.findById(req.session.userId)
-  .exec((error, user) => {
-    if (error) {
-      return next(error);
-    } else {
-      if (user === null) {
-        let err = new Error('Not Authorized!');
-        err.status = 400;
-        return next(err);
-      } else {
-        return res.send('Something Here...')
-      }
-    }
-  });
-});
-
-router.get('/logout', (req, res, next) => {
-  if (req.session) {
-    req.session.destroy((err) => {
-      if (err) {
-        return next(err);
-      } else {
-        return res.redirect('/');
-      }
-    });
-  }
-});
-
-///////////////////////////////////////////
-// router.get('/login', (req, res) => {
-//   login.login(req.body)
-//   .then((userToValidate) => {
-//     if(isValidUser === true) {
-//       res.status(200).send(isValidUser);
-//     } else {
-//       res.status(404).send('User Not Found!');
+// router.post('/login', (req, res, next) => {
+//   if (req.body.password !== req.body.passwordConfirm) {
+//     let err = new Error('Passwords do not match!');
+//     err.status = 400;
+//     res.send('Passwords do not match!');
+//     return next(err);
+//   }
+//
+//   if (req.body.email && req.body.username && req.body.password && req.body.passwordConfirm) {
+//     let userData = {
+//         email: req.body.email,
+//         username: req.body.username,
+//         password: req.body.password,
+//         passwordConfirm: req.body.passwordConfirm
 //     }
-//   })
-//   .catch((err) => {
-//     res.status(500).send(err);
+//
+//     User.create(userData, (err, user) => {
+//       if (error) {
+//         return next(error);
+//       } else {
+//         req.session.userId = user._id;
+//         return res.redirect('/userProfile');
+//       }
+//     });
+//   } else if (req.body.logemail, req.body.logpassword, (error, user) => {
+//     User.authenticated(req.body.logemail, req.body.logpassword, (error, user) => {
+//       if (error || !user) {
+//         let err = new Error('Wrong email or password!');
+//         err.status = 401;
+//         return next(err);
+//       } else {
+//         req.session.userId = user._id;
+//         return res.redirect('/userProfile');
+//       }
+//     });
 //   });
 // });
+//
+// router.get('/userProfile', (req, res, next) => {
+//   User.findById(req.session.userId)
+//   .exec((error, user) => {
+//     if (error) {
+//       return next(error);
+//     } else {
+//       if (user === null) {
+//         let err = new Error('Not Authorized!');
+//         err.status = 400;
+//         return next(err);
+//       } else {
+//         return res.send('Something Here...')
+//       }
+//     }
+//   });
+// });
+//
+// router.get('/logout', (req, res, next) => {
+//   if (req.session) {
+//     req.session.destroy((err) => {
+//       if (err) {
+//         return next(err);
+//       } else {
+//         return res.redirect('/');
+//       }
+//     });
+//   }
+// });
+
+///////////////////////////////////////////
+router.get('/login', (req, res) => {
+  login.login(req.body)
+  .then((userToValidate) => {
+    if(isValidUser === true) {
+      res.status(200).send(isValidUser);
+    } else {
+      res.status(404).send('User Not Found!');
+    }
+  })
+  .catch((err) => {
+    res.status(500).send(err);
+  });
+});
 
 // Delete User
 router.delete('/deleteUser', (req, res) => {
