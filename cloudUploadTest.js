2d65b592565434ba3a270c9b050ee9370f1fc580
MrMPerial <christopher.mitchell26@gmail.com>
2017-11-13 11:35:53 -0600

    Tested and formatted object upload code

--- cloudUploadTest.js
+++ cloudUploadTest.js
@@ -1,12 +1,73 @@
 let cloudinary = require('cloudinary');
 
-cloudinary.config({
-  cloud_name: 'mperial-web-solutions',
-  api_key: '938561814326735',
-  api_secret: 'QPUJetWRNBfDEjfIcZUnXCft6vM'
+// Profile Pic
+cloudinary.v2.uploader.upload('{{ file path }}',
+{public_id: "{{ username }}",
+transformation: [
+  {
+    width: 125,
+    height: 125,
+    crop: "scale"
+  }
+]
+},
+(error, result) => {
+  if(error) {
+    console.log(error);
+    res.status(500).send('Something went wrong while uploading image. Please Try again.')
+  }
+  console.log(result);
+  res.status(200).send('Image uploaded Successfully.')
 });
 
+// Song Cover
+cloudinary.v2.uploader.upload('{{ file path }}',
+{public_id: "{{ username + song title }}",
+transformation: [
+  {
+    width: 300,
+    height: 300,
+    crop: "scale"
+  }
+]
+},
+(error, result) => {
+  if(error) {
+    console.log(error);
+    res.status(500).send('Something went wrong while uploading image. Please Try again.')
+  }
+  console.log(result);
+  res.status(200).send('Image uploaded Successfully.')
+});
 
-cloudinary.uploader.upload("marlowProPic.png", function(result) {
+// Banner
+cloudinary.v2.uploader.upload('{{ file path }}',
+{public_id: "{{ username + path name }}",
+transformation: [
+  {
+    width: 1200,
+    height: 300,
+    crop: "scale"
+  }
+]
+},
+(error, result) => {
+  if(error) {
+    console.log(error);
+    res.status(500).send('Something went wrong while uploading image. Please Try again.')
+  }
+  console.log(result);
+  res.status(200).send('Image uploaded Successfully.')
+});
+
+// Song
+cloudinary.v2.uploader.upload("Lift_Off_Single.mp3",
+{resource_type: "video",
+public_id: "{{ username + song title }}"
+},
+(error, result) => {
+  if(error) {
+    console.log(error);
+  }
   console.log(result);
 });
