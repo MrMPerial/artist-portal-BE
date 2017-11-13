553b4b56de198715740a3b1ec5d9573e4bac1360
MrMPerial <christopher.mitchell26@gmail.com>
2017-11-13 13:17:49 -0600

    Re-Organizing

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1183972921705936',
+        'clientSecret'  : 'f8376e4fd45dacf715336a37a8acad89',
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
-const routes = require('./routes');
+// const routes = require('./routes');
+const passport = require('passport');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 
+const routes = require('./routes')(passport);
+const configPassport = require('./configs/passport.config');
 const mongodb = require('./mongodb.utils');
 
 mongodb.createEventListeners();
 mongodb.connect();
 
+configPassport(passport);
 const app = express();
 
+app.use(session({ secret: 'menagerie of monkeys', resave: false, saveUninitialized: false }));
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/assets', express.static(process.cwd() + '/assets'));
 app.use('/', routes);
 
 app.listen(5000, () => {
