c8a64cc7aa06904e533290cd2e0326fd381c283f
MrMPerial <christopher.mitchell26@gmail.com>
2017-11-13 13:19:32 -0600

    Updated User Model

--- models/user.model.js
+++ models/user.model.js
@@ -1,29 +1,9 @@
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-  },
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  passwordConfirm: {
-    type: String,
-    required: true
-  },
-  profileType: {
-    type: String
-  }
+  id: String
+  username: String
+  profileType: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
