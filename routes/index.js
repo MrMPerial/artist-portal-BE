66bc5fc270dd73091976dbed87fe1f5c82caef50
MrMPerial <christopher.mitchell26@gmail.com>
2017-11-13 15:55:47 -0600

    Consolidated signup and login

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-const signup = require('./signup');
 const users = require('./users');
 const profiles = require('./profiles');
 const songs = require('./songs');
@@ -9,7 +8,6 @@ router.get('/', (req, res) => {
   res.status(200).send('Looks like we\'re up and running!');
 });
 
-router.use('/signup', signup);
 router.use('/users', users);
 router.use('/profiles', profiles);
 router.use('/songs', songs);
--- routes/users/index.js
+++ routes/users/index.js
@@ -2,11 +2,25 @@ const router = require('express').Router();
 
 const mongodb = require('../../utils/mongodb.utils');
 
-// User Requirements
 const login = require('./login');
 const deleteUser = require('./deleteUser');
+const addUser = require('./addUser');
 
 // User Endpoints //
+
+// TODO: Add oAuth
+
+// Add Artist
+router.post('/addArtist', (req, res) => {
+  addUser.addUser(req, 'Artist');
+  res.status(200).send('Successfully added new artist!');
+});
+
+// Add Fan
+router.post('/addFan', (req, res) => {
+  addUser.addUser(req, 'Fan');
+  res.status(200).send('Successfully added new fan!');
+});
 
 // User Login
 router.get('/login', (req, res) => {
