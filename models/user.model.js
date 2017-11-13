5e93fda0765aff88e6e27274fd6161b779af1c68
MrMPerial <christopher.mitchell26@gmail.com>
2017-11-13 13:27:12 -0600

    Added Demo "Github" Code

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
-const UserSchema = mongoose.Schema({
-  id: String
-  username: String
-  profileType: String
+// const UserSchema = mongoose.Schema({
+//   id: String
+//   username: String
+//   profileType: String
+// });
+
+const userSchema = new Schema({
+  github: {
+    id: String,
+    username: String,
+    publicRepos: Number
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- routes/signup/index.js
+++ routes/signup/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const isLoggedIn = require('../middleware/is-logged-in.mw');
 
 const mongodb = require('../../mongodb.utils');
 
@@ -6,6 +7,33 @@ const mongodb = require('../../mongodb.utils');
 const addUser = require('./addUser');
 
 // TODO: Add oAuth
+
+module.exports = function (passport) {
+  var router = require('express').Router();
+
+  router.get('/', (req, res) => {
+    res.status(200).sendFile(process.cwd() + '/assets/index.html');
+  });
+
+  router.get('/login', passport.authenticate('github', { scope: ['user:email'] }));
+
+  router.get(
+    '/login/callback',
+    passport.authenticate('github', { failureRedirect: '/' }),
+    (req, res) => { res.redirect('/secrets');
+  });
+
+  router.get('/secrets', isLoggedIn, function (req, res) {
+    res.status(200).sendFile(process.cwd() + '/assets/secrets.html');
+  });
+
+  router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  return router;
+};
 
 // Signup Endpoints //
 
